61e2a86861e28c8386aa6e23f02fa134e18643f8
Icebob <mereg.norbert@gmail.com>
2017-06-03 23:59:01 +0200

    add seed service

---
+++ services/seed.service.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+	name: "seed-db",
+
+	// Exposed actions
+	actions: {
+
+	},
+
+	// Event listeners
+	events: {
+
+	},
+
+	// Service methods
+	methods: {
+
+	},
+
+	created() {
+		// this.logger.info("Service created!");
+	},
+
+	started() {
+		// this.logger.info("Service started!");
+	},
+
+	stopped() {
+		// this.logger.info("Service stopped!");
+	}
+};
